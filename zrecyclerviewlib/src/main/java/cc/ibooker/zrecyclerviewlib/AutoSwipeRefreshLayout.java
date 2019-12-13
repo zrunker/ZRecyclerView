f047110b1ff4e279307a957c68767524a2523c80
zrunker <zrunker@yahoo.com>
2019-12-13 11:25:00 +0800

    init Commit

--- zrecyclerviewlib/src/main/java/cc/ibooker/zrecyclerviewlib/AutoSwipeRefreshLayout.java
+++ zrecyclerviewlib/src/main/java/cc/ibooker/zrecyclerviewlib/AutoSwipeRefreshLayout.java
@@ -1,6 +1,8 @@
 package cc.ibooker.zrecyclerviewlib;
 
 import android.content.Context;
+import android.support.annotation.NonNull;
+import android.support.annotation.Nullable;
 import android.support.v4.widget.SwipeRefreshLayout;
 import android.util.AttributeSet;
 import android.view.View;
@@ -13,18 +15,50 @@ import java.lang.reflect.Method;
  * Created by 邹峰立 on 2017/5/10.
  */
 public class AutoSwipeRefreshLayout extends SwipeRefreshLayout {
-    public AutoSwipeRefreshLayout(Context context) {
-        super(context);
+    private OnRefreshListener onRefreshListener;
+
+    public AutoSwipeRefreshLayout(@NonNull Context context) {
+        this(context, null);
     }
 
-    public AutoSwipeRefreshLayout(Context context, AttributeSet attrs) {
+    public AutoSwipeRefreshLayout(@NonNull Context context, @Nullable AttributeSet attrs) {
         super(context, attrs);
+        init();
+    }
+
+    // 初始化
+    private void init() {
+        setColorSchemeResources(android.R.color.holo_blue_bright,
+                android.R.color.holo_green_light,
+                android.R.color.holo_orange_light,
+                android.R.color.holo_red_light);
+    }
+
+    @Override
+    public void setOnRefreshListener(@Nullable OnRefreshListener listener) {
+        if (listener != null)
+            onRefreshListener = listener;
+        super.setOnRefreshListener(listener);
+    }
+
+    /**
+     * 取消下拉刷新监听
+     */
+    public void setCanRefresh(boolean isCanRefresh) {
+        if (isCanRefresh) {
+            setOnRefreshListener(onRefreshListener);
+            setRefreshing(false);
+        } else {
+            setOnRefreshListener(null);
+            setRefreshing(true);
+        }
     }
 
     /**
      * 自动刷新
      */
     public void autoRefresh() {
+        // 修改界面
         try {
             Field mCircleView = SwipeRefreshLayout.class.getDeclaredField("mCircleView");
             mCircleView.setAccessible(true);
@@ -37,5 +71,8 @@ public class AutoSwipeRefreshLayout extends SwipeRefreshLayout {
         } catch (Exception e) {
             e.printStackTrace();
         }
+        // 执行刷新事件
+        if (onRefreshListener != null)
+            onRefreshListener.onRefresh();
     }
 }
